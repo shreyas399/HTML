c67383532d18d49adebe5c3f38261e09e803fbbb
shreyas399 <129728792+shreyas399@users.noreply.github.com>
2023-07-27 17:28:50 +0530

    Create Horizontal tag.java

---
+++ Horizontal tag.java
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <title>
+        Start page
+    </title>
+    <head>
+        <body>
+        <h1>Hello world</h1>
+        <br/>
+
+       <big> To make the text big</big>
+       <br />
+       <hr />                         <!-- Uesd to make some space in between-->
+       <small> To make the text smallar</small>
+        </body>
+    </head>
+
+</html>
